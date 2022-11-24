cdf6b1c1f1f3a22831808a19cffb3bf452610e60
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-11-24 12:09:08 -0500

    cleaned up gitignore

--- ANEWFILE_created_twice.php
+++
@@ -1,9 +0,0 @@
-<?php
-            $x == 1 ||
-    function unbounded_unproven($x, $oddCoefficient, $oddAddend, $evenDivisor)
-    {
-        if (
-        // (4, 6, 2)
-        ($oddCoefficient == 4 && $oddAddend == 6 && $evenDivisor == 2
-        && (
-            $x == 1 ||
--- test.mpl
+++
@@ -1,3 +0,0 @@
-2^k*x + 2^k*m + 2^k - l;
-simplify(% / 2);
-simplify(%);
